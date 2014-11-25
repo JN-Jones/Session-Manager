40c555f128d72b265df967901af497822249e9a9
ATofighi <alirtofighim@gmail.com>
2014-11-25 15:15:32 +0330

    Translate to Persian

---
+++ inc/languages/persian/sessionmanager.lang.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ *
+ * Plugin By: Jones
+ * Language: Persian
+ * Translator: AliReza Tofighi - http://my-bb.ir
+ *
+ */
+
+$l['sessions'] = "نشست‌ها";
+$l['location'] = "موقوعیت (IP)";
+$l['browser'] = "مروگر";
+$l['page'] = "صفحه";
+$l['lastseen'] = "آخرین بازدید";
+$l['revoke'] = "لغو کردن";
+$l['sessionclosed'] = "نشست بسته‌شد.";
+$l['activesession'] = "نشست فعال است";
